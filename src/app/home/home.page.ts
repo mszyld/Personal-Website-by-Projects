bb3c795a5d1698b32c121c87ebe9cf35774f78c0
mszyld <mszyld@gmail.com>
2022-10-29 16:31:28 -0300

    Init checking of ion-accordion

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,19 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public name = "Martin";
+  public articles = ["Awesome Article 1", "Great Article 2"];
+  public projects = ["Awesome Project 9000", "I buillt my own page"];
+  public currentList: string[] = this.projects;
+
   constructor() {}
+
+  itemCLicked(item) {
+    console.log("Maigc!!!", item);
+  }
+
+  segmentHasChanged(value) {
+    this.currentList = value.detail.value;
+  }
 
 }
